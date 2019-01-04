357b098493f647ac8ab7a7f4542bbe974e22f0a4
Your Name <you@example.com>
2019-01-04 20:52:46 +0900

    delete error

--- Gemfile
+++ Gemfile
@@ -88,3 +88,4 @@ gem 'therubyracer'
 gem 'bootstrap-sass', '~> 3.3.6'
 gem 'momentjs-rails', '>= 2.9.0'
 gem 'bootstrap3-datetimepicker-rails', '~> 4.17.37'
+
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def default_meta_tags
+    {
+        title: "スマホ自動コールシステムSMART CTI",
+    	site: "SMART CTI",
+    	description: "スマホで自動コールシステムならSMART CTI。通話料が定額のため、今までより大きくコストカットが出来ます。",
+    	keywords: "スマホ CTI",
+        noindex: ! Rails.env.production?, # production環境以外はnoindex  
+        charset: "UFT-8",
+    }
+    end
+
 end
